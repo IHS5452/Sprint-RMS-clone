d26e334cee267b0d29a4bdbd4387ee1f4ed56b56
Ian Schrauth <ianschrauth@sbcglobal.net>
2026-02-01 14:30:05 -0600

    i

--- src/rms/clone/vars/db.java
+++ src/rms/clone/vars/db.java
@@ -11,8 +11,8 @@ package rms.clone.vars;
 public class db {
        public static final String DB_URL = "jdbc:mysql://localhost:3306/";
     public static String DB_NAME = "rms_clone";
-    public static final String DB_USER = "ideuser";
-    public static final String DB_PASSWORD = "idepasswd123";
+    public static final String DB_USER = "root";
+    public static final String DB_PASSWORD = "passwd123";
  
    public static final String CLASS_FILE = "com.mysql.cj.jdbc.Driver";
 }
